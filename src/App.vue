c9103bd1c621227ef75ef8a068d49b77571eb3d3
Rockmate77 <52208570+Rockmate77@users.noreply.github.com>
2023-09-25 14:21:08 +0200

    Ex 3: remove vue CLI placeholders

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>deleted helloworld</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
