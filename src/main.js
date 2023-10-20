42bc9c61a911b47c3650b53face68fddd2151e77
Rockmate77 <52208570+Rockmate77@users.noreply.github.com>
2023-10-20 15:14:45 +0200

    Ex 8: add guards for authenticated routes

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
-import { createApp } from 'vue'
+import { createApp} from 'vue'
 import App from './App.vue'
-import store from './lib/store'
 import homePage from "@/pages/HomePage.vue";
 import ConversationsIndexPage from "@/pages/ConversationsIndexPage.vue";
 import { createRouter, createWebHashHistory } from 'vue-router';
-
-
+import store from '@/lib/store'
 const routes = [
     { path: '/', component: homePage },
-    { path: '/conv', component: ConversationsIndexPage },
+    {
+        path: '/conv',
+        name: 'Conv',
+        component: ConversationsIndexPage,
+        // Add a beforeEnter guard to check if user is null
+        beforeEnter: (to, from, next) => {
+            // Replace 'user' with your actual user authentication check
+            if (store.state.user === null) {
+                // User is null, do not allow access to '/con'
+                next('/'); // Redirect to the home page or another appropriate route
+            } else {
+                // User is authenticated, allow access to '/con'
+                next();
+            }
+        }
+    }
 ]
 const router = createRouter({
     history: createWebHashHistory(),
